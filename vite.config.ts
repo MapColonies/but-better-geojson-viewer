fff753dceeeae02dedc678d383091a0510e5c5b0
Niv Greenstein <88280771+NivGreenstein@users.noreply.github.com>
2026-02-11 05:30:37 +0200

    ci: wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const base = process.env.VITE_BASE_PATH ?? process.env.BASE_URL ?? '/';
+
 export default defineConfig({
 	plugins: [react()],
-	base: '/but-better-geojson-viewer',
+	base,
 });
